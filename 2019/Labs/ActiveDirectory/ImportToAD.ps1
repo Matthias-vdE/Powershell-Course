a9e3556efe3bd20eaf8f19d637a10058a6cb96ef
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2019-08-19 13:59:47 +0200

    AD

---
+++ 2019/Labs/ActiveDirectory/ImportToAD.ps1
@@ -0,0 +1,6 @@
+﻿$Users = Import-Csv ADImport.csv
+
+foreach ($User in $Users){
+
+    New-ADUser -Name "$($User.GivenName) $($User.Surname)" -GivenName $User.GivenName -Surname $User.Surname -OtherAttribute @{telephoneNumber=$User.TelephoneNumber} -AccountPassword (ConvertTo-SecureString ($User.Password) -AsPlainText -Force) -Enabled $true
+}
